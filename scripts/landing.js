02a8ebb4b20cb3fe0b15c0dbe461ba92c14fc127
Zach Stradling <zachstradling@gmail.com>
2016-09-13 16:17:32 -0700

    refactored landing.js with a for loop

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,32 +2,20 @@ alert("Why hello there! I'm a wee bit of JavaScript.")
              var animatePoints = function() {
  
                  var points = document.getElementsByClassName('point');
- 
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
 
-             
+ 
+             var revealPoint = function() {
+
+             for (var i = 0; i < points.length; i++) {
+
+                points[i].style.opacity = 1;
+                points[i].style.transform = "scaleX(1) translateY(0)";
+                points[i].style.msTransform = "scaleX(1) translateY(0)";
+                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+             }
+                }; 
+                revealPoint();
+
+             };
+             animatePoints();
