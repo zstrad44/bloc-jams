c24f65d462b4b5a3521ffb8e484669d4b7b7634e
Zach Stradling <zachstradling@gmail.com>
2016-09-13 17:52:07 -0700

    assignmen-9 added utulities.js. Updated landing.js with forEach function. removed for loop.

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,22 +3,20 @@
 
         var pointsArray = document.getElementsByClassName('point');
 
-             var animatePoints = function(points) {
- 
-             var revealPoint = function() {
+             
+            var revealPoint = function(point) {
 
-             for (var i = 0; i < points.length; i++) {
-
-                points[i].style.opacity = 1;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-
-             }
-                }; 
-                revealPoint();
-
+                point.style.opacity = 1;
+                point.style.transform = "scaleX(1) translateY(0)";
+                point.style.msTransform = "scaleX(1) translateY(0)";
+                point.style.WebkitTransform = "scaleX(1) translateY(0)";
              };
+
+            var animatePoints = function(points) {
+                forEach(points, revealPoint);
+             };
+
+
              
  window.onload = function() {
 
