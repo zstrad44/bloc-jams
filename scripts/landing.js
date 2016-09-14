4e989d27d55e0817b6c696032c4780d85888ca3c
Zach Stradling <zachstradling@gmail.com>
2016-09-13 17:00:11 -0700

    added event listener for scroll effects in landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,9 @@
-alert("Why hello there! I'm a wee bit of JavaScript.")
-             var animatePoints = function() {
- 
-                 var points = document.getElementsByClassName('point');
 
+
+
+        var pointsArray = document.getElementsByClassName('point');
+
+             var animatePoints = function(points) {
  
              var revealPoint = function() {
 
@@ -18,4 +19,19 @@ alert("Why hello there! I'm a wee bit of JavaScript.")
                 revealPoint();
 
              };
-             animatePoints();
+             
+ window.onload = function() {
+
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+var sellingPoints = document.getElementsByClassName('selling-points')[0];
+var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+         });
+ };
