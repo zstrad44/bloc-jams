1ae198979705a44e692e16edcd6390aef1981014
Zach Stradling <zachstradling@gmail.com>
2016-09-15 16:24:50 -0700

    assignment 11 done

--- scripts/album.js
+++ scripts/album.js
@@ -31,6 +31,22 @@ var albumPicasso = {
      ]
  };
 
+ // Another Example Album
+ var albumBlink182 = {
+     title: 'Miss You',
+     artist: 'Blink 182',
+     label: 'Zach Stradling',
+     year: '2001',
+     albumArtUrl: 'assets/images/album_covers/Blink182AlbumCover.jpg',
+     songs: [
+         { title: 'Hello?', duration: '18:01' },
+         { title: 'Ring, rang, ring', duration: '15:01' },
+         { title: 'Hot pocket', duration: '13:21'},
+         { title: 'Can you hear me now? Verizon', duration: '31:14' },
+         { title: 'Wrong phone number, thank you!', duration: '21:15'}
+     ]
+ };
+
 
 
  var createSongRow = function(songNumber, songName, songLength) {
@@ -45,15 +61,16 @@ var albumPicasso = {
      return template;
  };
 
-
-
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ 	var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+
+ var setCurrentAlbum = function(album) {
+     // #1
+    
  
      // #2
      albumTitle.firstChild.nodeValue = album.title;
@@ -72,6 +89,17 @@ var albumPicasso = {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumBlink182];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+     	setCurrentAlbum(albums[index]);
+     	index++;
+
+     	if (index == albums.length) {
+     		index = 0;
+     	}
+     });
  };
 
 
