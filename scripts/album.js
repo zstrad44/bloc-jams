355ae2233c1fcc1d750452680593ee9f137cb145
Zach Stradling <zachstradling@gmail.com>
2016-09-15 18:41:46 -0700

    assignment-13-DOM-Scripting

--- scripts/album.js
+++ scripts/album.js
@@ -118,6 +118,14 @@ var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className != targetClass && currentParent.className !== null) {
+
+     		if (currentParent == null) {
+     			alert("No parent found");
+
+     		} else if ( currentParent.className !== targetClass) {
+     				alert("No parent found with that class name");
+     		}
+
             currentParent = currentParent.parentElement;
         }
         return currentParent;
