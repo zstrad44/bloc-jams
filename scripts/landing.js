7da37eeb5d32223c171274bdfd99887db0491eea
Zach Stradling <zachstradling@gmail.com>
2016-09-22 18:43:18 -0700

    Assignment 8 refactored

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,19 @@
-alert("Why hello there! I'm a wee bit of JavaScript.")
-             var animatePoints = function() {
+
+ var animatePoints = function() {
+
+    var points = document.getElementsByClassName('point');
+
+    var revealPoint = function(index) {
+
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+    }
  
-                 var points = document.getElementsByClassName('point');
+    for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
 
- 
-             var revealPoint = function() {
-
-             for (var i = 0; i < points.length; i++) {
-
-                points[i].style.opacity = 1;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-
-             }
-                }; 
-                revealPoint();
-
-             };
-             animatePoints();
+    }
+};
